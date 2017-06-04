e1eb5f9aa4ba1700e47f1cdda7259b173d686bce
Adeilson Silva <adeilsonsilva@dcc.ufba.br>
2017-06-04 01:15:39 -0300

    inicia form woman

---
+++ app/Http/Controllers/WomanController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Woman;
+use Illuminate\Http\Request;
+
+class WomanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('woman/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if (Woman::create($request->all())) {
+            return redirect()
+                ->back()
+                ->withSuccess('Assistida adicionada com sucesso!');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Woman  $woman
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Woman $woman)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Woman  $woman
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Woman $woman)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Woman  $woman
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Woman $woman)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Woman  $woman
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Woman $woman)
+    {
+        //
+    }
+}
